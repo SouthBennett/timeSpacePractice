45fecd39ac3ce00ae55a948ebf6cf26e16abad94
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-01-07 14:54:43 -0800

    Added three Junit Test for mostCommonTimeEfficient

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -7,4 +7,32 @@ public class PracticeTest {
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+
+  /** Test for mostCommonTimeEfficient */
+  @Test
+  public void testMostCommonEasy() {
+    int [] nums = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5};
+    
+    int result = Practice.mostCommonTimeEfficient(nums);
+
+    assertEquals(5, result); // 5 is the most common frequency
+  }
+
+  @Test
+  public void testMostCommonTieBreak() {
+    int [] nums = {2, 1, 2, 1};
+
+    int result = Practice.mostCommonTimeEfficient(nums);
+
+    assertEquals(2, result); // Both have a frequency of 2 but the number 2 shows up first. 
+  }
+
+  @Test
+  public void testASingleElement() {
+    int [] nums = {24};
+
+    int result = Practice.mostCommonTimeEfficient(nums);
+
+    assertEquals(42, result);
+  }
 }
